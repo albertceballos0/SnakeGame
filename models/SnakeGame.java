0f09501c323c01ce5fe839fba4df1208ea51cc76
albertceballos0 <albertceballos37@gmail.com>
2024-10-16 14:30:45 +0200

    implement methods about SnakeGame

---
+++ models/SnakeGame.java
@@ -0,0 +1,69 @@
+package models;
+
+import java.util.Random;
+
+public class SnakeGame {
+    private SnakeBoard board;
+    private SnakePlayer player;
+    private SnakeFood food;
+    private boolean isGameOver;
+    private int score;
+
+    public SnakeGame(int width, int height) {
+    
+        this.board = new SnakeBoard(width, height);
+        this.player = new SnakePlayer(width / 2, height / 2);
+        this.food = new SnakeFood(0, 0);
+        this.isGameOver = false;
+        this.score = 0;
+        placeFood();
+
+    }
+
+    public void restart() {
+        this.player = new SnakePlayer(board.getWidth() / 2, board.getHeight() / 2);
+        this.food = new SnakeFood(0, 0);
+        this.isGameOver = false;
+        this.score = 0;
+        placeFood();
+    }
+
+    public void update() {
+        if (!isGameOver) {
+            player.move();
+            checkCollision();
+            checkFood();
+        }
+    }
+
+    private void checkCollision() {
+        if (player.collidesWithWall(board) || player.collidesWithSelf()) {
+            isGameOver = true;
+        }
+    }
+
+    private void checkFood() {
+        if (player.getX() == food.getX() && player.getY() == food.getY()) {
+            player.grow();
+            score++;
+            placeFood();
+        }
+    }
+
+    private void placeFood() {
+        Random rand = new Random();
+        int x, y;
+        do {
+            x = rand.nextInt(board.getWidth());
+            y = rand.nextInt(board.getHeight());
+        } while (player.occupies(x, y));
+        food.setPosition(x, y);
+    }
+
+    // Getters and setters
+    public SnakeBoard getBoard() { return board; }
+    public SnakePlayer getPlayer() { return player; }
+    public SnakeFood getFood() { return food; }
+    public boolean isGameOver() { return isGameOver; }
+    public int getScore() { return score; }
+}

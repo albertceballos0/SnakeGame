a1f515c1af554784d9275a1da5c9a21c0607c2d6
albertceballos0 <albertceballos37@gmail.com>
2024-10-16 14:29:51 +0200

    Add SnakeBoard class to represent the game board dimensions

---
+++ models/SnakeBoard.java
@@ -0,0 +1,16 @@
+package models;
+
+
+public class SnakeBoard {
+    private int width;
+    private int height;
+
+    public SnakeBoard(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    // Getters
+    public int getWidth() { return width; }
+    public int getHeight() { return height; }
+}

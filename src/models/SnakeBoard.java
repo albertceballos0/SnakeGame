8aa4456517888be868ba92170b51f78f781603ba
albertceballos0 <albertceballos37@gmail.com>
2024-10-16 12:27:28 +0200

    Refactorizar clase SnakeBoard para agregar métodos de acceso y modificación de la comida

--- src/models/SnakeBoard.java
+++ src/models/SnakeBoard.java
@@ -3,7 +3,29 @@ import java.util.LinkedList;
 
 public class SnakeBoard {
 
-    //private int width, height;
-    //private SnakeFood food;   
-    //private SnakeObstacles[] obstacles;
+    private int width, height;
+    private SnakeFood food;   
+
+    public SnakeBoard(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.food = new SnakeFood();
+
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public SnakeFood getFood() {
+        return food;
+    }   
+
+    public void setFood(SnakeFood food) {
+        this.food = food;
+    }
 }

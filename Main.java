3e506fffd4e9b983125c9089a900b2056c88e597
albert ceballos <albert.ceballos@somosinapsis.com>
2024-11-25 18:11:06 +0100

    Remove innecesary files

--- Main.java
+++
@@ -1,9 +0,0 @@
-import views.GameSetUp;
-
-
-public class Main {
-    public static void main(String[] args) {
-        GameSetUp setup = new GameSetUp();
-        setup.showSetupDialog();
-    }
-}
--- models/SnakeObstacles.java
+++
@@ -1,5 +0,0 @@
-package models;
-
-public class SnakeObstacles {
-    //private int width, height;
-}

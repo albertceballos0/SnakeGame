49628164c7382f2f5df1f4f49af6a5646824fc8b
albert ceballos <albert.ceballos@somosinapsis.com>
2024-11-25 18:11:21 +0100

    feat: agregar clase Main para iniciar el juego de la serpiente

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+import controllers.SnakeController;
+
+
+public class Main {
+    public static void main(String[] args) {
+        SnakeController controller = new SnakeController();
+        controller.start();
+    }
+}

4f9a4c269e13ff112070febd7ed586e414b33e04
albert ceballos <albert.ceballos@somosinapsis.com>
2024-11-25 18:12:58 +0100

    feat: agregar clase SnakeFood para gestionar la lógica de la comida en el juego de la serpiente

---
+++ src/models/SnakeFood.java
@@ -0,0 +1,15 @@
+package models;
+
+public class SnakeFood {
+    private int x;
+    private int y;
+
+    public SnakeFood(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // Getters
+    public int getX() { return x; }
+    public int getY() { return y; }
+}

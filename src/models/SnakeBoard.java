bd42b6998c323428b2bed3e4de2d6017a7fd5df9
1603295 <gerardasbert@gmail.com>
2024-12-01 20:10:43 +0100

    Design by contract in SnakeBoard

--- src/models/SnakeBoard.java
+++ src/models/SnakeBoard.java
@@ -12,6 +12,10 @@ public class SnakeBoard {
 
 
     public SnakeBoard(int width, int height) {
+        // Design by contract
+        if (width <= 0 || height <= 0 || width >= 500 || height >= 500) {
+            throw new IllegalArgumentException("Dimensions de la Board han de ser positives i menors a 500");
+        }
         this.width = width;
         this.height = height;
         this.obstacles = new ArrayList<>();
@@ -25,6 +29,7 @@ public class SnakeBoard {
     }
 
     public boolean  isObstacle(int x, int y) {
+        // No es necessari design by contract
         for (SnakeObstacle obstacle : obstacles) {
             if (obstacle.getX() == x && obstacle.getY() == y) {
                 return true;
@@ -34,14 +39,23 @@ public class SnakeBoard {
     }
 
     public void setObstacle(int x,int y) {
+        // Design by contract
+        if (x < 0 || x >= width || y < 0 || y >= height) {
+            throw new IllegalArgumentException("Coordenades de l'Obstacle han d'estar dintre de la Board");
+        }
         obstacles.add(new SnakeObstacle(x, y));   
     }
 
     public boolean isFood(int x, int y) {
+        // No es necessari design by contract
         return food.getX() == x && food.getY() == y;
     }
 
     public void setFood(int x, int y) {
+        // Design by contract
+        if (x < 0 || x >= width || y < 0 || y >= height) {
+            throw new IllegalArgumentException("Coordenades de la Food han d'estar dintre de la Board");
+        }
         food = new SnakeFood(x, y);
     }
     // Getters

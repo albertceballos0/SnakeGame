29a114772fae7fac6d9220d65907befd52e9eb0d
albert ceballos <albert.ceballos@somosinapsis.com>
2024-12-01 22:13:57 +0100

    Eliminar mensaje de inicio del juego en la clase Main

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ import controllers.SnakeController;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Snake Game");
         SnakeController controller = new SnakeController();
         controller.start();
     }

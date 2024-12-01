9e61c1112f20e48379947cc11b35a31b7ae2ed1d
1603295 <gerardasbert@gmail.com>
2024-12-01 20:11:29 +0100

    Design by contract in SnakeObstacle

--- src/models/SnakeObstacle.java
+++ src/models/SnakeObstacle.java
@@ -7,6 +7,10 @@ public class SnakeObstacle {
     private int y = 0;
     
     public SnakeObstacle(int x, int y) {
+        // Design by contract
+        if (x < 0 || y < 0) {
+            throw new IllegalArgumentException("Coordenades de l'Obstacle no poden ser negatives");
+        }
         this.x = x;
         this.y = y;
     }
